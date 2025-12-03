bb2155da1a9a9797704670d2bb5bfcd9a81b3aa0
“kathrinalbertinefreytag-commits” <kathrinalbertinefreytag@yahoo.com>
2025-12-03 21:33:06 +0100

    new version

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-def by-age():
+def by_age():
     while True:
-    age = input("What is your age?")
-    if age >0 < 10:
-        print("Enjoy your childhood")
-        break
-    elif age >10 and age <20:
-        print("Happay teenagehood")
-        break
-    elif age >20:
-        print("Congrats you are an adult")
-        break
-    else:
-        "Please enter a number"
+        try:
+            age = int(input("What is your age? "))
+        except ValueError:
+            print("Please enter a valid number!")
+            continue
+
+        if 0 < age < 10:
+            print("Enjoy your childhood")
+            break
+        elif 10 <= age < 20:
+            print("Happy teenagehood")
+            break
+        elif age >= 20:
+            print("Congrats, you are an adult")
+            break
+        else:
+            print("Please enter a positive number.")
+
+# Call
+if __name__ == "__main__":
+    by_age()
