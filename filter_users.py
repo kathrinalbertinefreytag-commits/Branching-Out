d945fd8243c764e5a234f5e2ab177e5e28f57093
“kathrinalbertinefreytag-commits” <kathrinalbertinefreytag@yahoo.com>
2025-12-19 15:35:14 +0100

    little mistake corrected

--- filter_users.py
+++ filter_users.py
@@ -12,7 +12,7 @@ def filter_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)
 
-    filtered_users = [user for user in users if user.get("age") == age_to_search]
+    filtered_users = [user for user in users if user.get("age") == age]
 
     if filtered_users:
         for user in filtered_users:
